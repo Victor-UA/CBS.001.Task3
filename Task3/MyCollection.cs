74094473c4acd083b1937dad2be6702c3fde4ee8
Victor <Victor_UA@Ukr.net>
2019-05-07 15:11:54 +0300

    Add project files.

---
+++ Task3/MyCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class MyCollection : ICollection
+    {
+        private int DEFAULT_CITIZENS_QUANTITY = 10;
+
+        private Citizen[] _citizens;
+
+        public MyCollection()
+        {
+            _citizens = new Citizen[DEFAULT_CITIZENS_QUANTITY];
+            Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public object SyncRoot => false;
+
+        public bool IsSynchronized => false;
+
+        public void CopyTo(Array array, int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                _citizens.CopyTo(array, index);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _citizens.GetEnumerator();
+        }
+    }
+}
